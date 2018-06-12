6e3e44f9d299215ea3fadc752e22ccebb0c63c1f
Kaja Mikolajczyk <kajaths@gmail.com>
2018-06-12 17:20:49 +0000

    Fiexd bug

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-test-kajamiko.c9users.io', 'blog-test-apeczka.heroku.com']
+ALLOWED_HOSTS = ['django-blog-test-kajamiko.c9users.io', 'blog-test-apeczka.herokuapp.com/']
 
 
 # Application definition
